9166bf7e72717e0ff6296ec12d7f513234d7b597
Mike van den Berg <m.vandenberg@picturae.com>
2023-05-30 12:46:53 +0200

    Composer update.

---
+++ bin/joomla-installer.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../../functions.php';
+
+use Symfony\Component\Console\Application;
+use Joomla\CMS\Installation\Console\InstallCommand;
+use Console\DownloadJoomlaCommand;
+
+$app = new Application();
+
+$app->add(new DownloadJoomlaCommand());
+
+$app->run();
